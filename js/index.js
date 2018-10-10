e03374fececb454bab1e98aa6902a355ceb4a74c
zjz <17683954109@163.com>
2018-10-10 14:52:51 +0800

    对js代码进行注释

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 
+//  监听用户输入enter，发送消息给 ws 服务器
+document.onkeydown=function (e) {
+    if (e.key==='Enter'){
+        send_message();
+    }
+};
 
+//  创建 WebSocket 连接
 let ws = new WebSocket('ws://127.0.0.1:9501');
+
+//  创建连接成功事件
 ws.onopen=function () {
     $_('tips').style.display='inline';
     $_('tips').innerHTML='连接成功';
@@ -9,9 +18,13 @@ ws.onopen=function () {
         $_('tips').style.display='none';
     },1500);
 };
+
+//  创建 WebSocket 接收到消息的回调函数
 ws.onmessage=function (data) {
     $_('msg').innerHTML+='<li class="msg_item">'+data.data+'</li>';
 };
+
+//  创建 WebSocket 连接关闭回调函数
 ws.onclose=function () {
     $_('tips').style.display='inline';
     $_('tips').innerHTML='连接断开';
@@ -21,6 +34,7 @@ ws.onclose=function () {
     },1500);
 };
 
+//  向 WebSocket 服务器发送消息
 function send_message() {
     let msg = $_('msg_from').value;
     if (msg!==''){
@@ -29,6 +43,7 @@ function send_message() {
     }
 }
 
+//  返回 id 的 dom 对象
 function $_(id) {
     return document.getElementById(id);
 }
